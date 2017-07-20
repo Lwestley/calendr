3ba1b9519d68d139313de4d690728367ac13d568
L.A. Westley <lutuawestley@gmail.com>
2017-07-19 20:41:12 -0600

    environment updates

--- server/env.js
+++ server/env.js
@@ -4,8 +4,8 @@
     It hosts configurations that is globally accessible via the process.env object. 
     It is used to host connectionstrings, server access keys and more. Generally we do not commit this file to a public repo, such as GitHub, because we may have sensitive data. Since we are using localhost, and no one can access our firewall unless we give them permission, we can commit it so that way in the future, we know our env variables are present.
  */
-process.env.PORT = 3000;
-process.env.DB = 'calendar';
-process.env.ENVIRONMENT = 'dev';
+if(!process.env.PORT) process.env.PORT = 3000;
+if(!process.env.DB) process.env.DB = 'mongodb://localhost:27017/calendar';
+if(!process.env.ENVIRONMENT) process.env.ENVIRONMENT = 'dev';
 
 // Notice we do not export these variables. `process` object is specific to Node and works out-of-the-box.
